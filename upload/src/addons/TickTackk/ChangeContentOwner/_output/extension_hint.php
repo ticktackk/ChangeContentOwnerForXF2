b17aa09554d8d2be275b064e8d8b55a7272ea218
TickTackk <me@ticktackk.io>
2018-05-10 04:52:20 +0530

    Clean up

--- upload/src/addons/TickTackk/ChangeContentOwner/_output/extension_hint.php
+++ upload/src/addons/TickTackk/ChangeContentOwner/_output/extension_hint.php
@@ -5,98 +5,44 @@
 
 namespace TickTackk\ChangeContentOwner\XFMG\Entity
 {
-    class XFCP_Album extends \XFMG\Entity\Album
-    {
-    }
-
-    class XFCP_Comment extends \XFMG\Entity\Comment
-    {
-    }
-
-    class XFCP_MediaItem extends \XFMG\Entity\MediaItem
-    {
-    }
+	class XFCP_Album extends \XFMG\Entity\Album {}
+	class XFCP_Comment extends \XFMG\Entity\Comment {}
+	class XFCP_MediaItem extends \XFMG\Entity\MediaItem {}
 }
 
 namespace TickTackk\ChangeContentOwner\XFMG\InlineMod
 {
-    class XFCP_Album extends \XFMG\InlineMod\Album
-    {
-    }
-
-    class XFCP_Comment extends \XFMG\InlineMod\Comment
-    {
-    }
-
-    class XFCP_Media extends \XFMG\InlineMod\Media
-    {
-    }
+	class XFCP_Album extends \XFMG\InlineMod\Album {}
+	class XFCP_Comment extends \XFMG\InlineMod\Comment {}
+	class XFCP_Media extends \XFMG\InlineMod\Media {}
 }
 
 namespace TickTackk\ChangeContentOwner\XFMG\Pub\Controller
 {
-    class XFCP_Album extends \XFMG\Pub\Controller\Album
-    {
-    }
-
-    class XFCP_Comment extends \XFMG\Pub\Controller\Comment
-    {
-    }
-
-    class XFCP_Media extends \XFMG\Pub\Controller\Media
-    {
-    }
+	class XFCP_Album extends \XFMG\Pub\Controller\Album {}
+	class XFCP_Comment extends \XFMG\Pub\Controller\Comment {}
+	class XFCP_Media extends \XFMG\Pub\Controller\Media {}
 }
 
 namespace TickTackk\ChangeContentOwner\XF\Entity
 {
-    class XFCP_Forum extends \XF\Entity\Forum
-    {
-    }
-
-    class XFCP_Post extends \XF\Entity\Post
-    {
-    }
-
-    class XFCP_ProfilePost extends \XF\Entity\ProfilePost
-    {
-    }
-
-    class XFCP_Thread extends \XF\Entity\Thread
-    {
-    }
+	class XFCP_Forum extends \XF\Entity\Forum {}
+	class XFCP_Post extends \XF\Entity\Post {}
+	class XFCP_ProfilePost extends \XF\Entity\ProfilePost {}
+	class XFCP_Thread extends \XF\Entity\Thread {}
 }
 
 namespace TickTackk\ChangeContentOwner\XF\InlineMod
 {
-    class XFCP_Post extends \XF\InlineMod\Post
-    {
-    }
-
-    class XFCP_ProfilePost extends \XF\InlineMod\ProfilePost
-    {
-    }
-
-    class XFCP_Thread extends \XF\InlineMod\Thread
-    {
-    }
+	class XFCP_Post extends \XF\InlineMod\Post {}
+	class XFCP_ProfilePost extends \XF\InlineMod\ProfilePost {}
+	class XFCP_Thread extends \XF\InlineMod\Thread {}
 }
 
 namespace TickTackk\ChangeContentOwner\XF\Pub\Controller
 {
-    class XFCP_InlineMod extends \XF\Pub\Controller\InlineMod
-    {
-    }
-
-    class XFCP_Post extends \XF\Pub\Controller\Post
-    {
-    }
-
-    class XFCP_ProfilePost extends \XF\Pub\Controller\ProfilePost
-    {
-    }
-
-    class XFCP_Thread extends \XF\Pub\Controller\Thread
-    {
-    }
+	class XFCP_InlineMod extends \XF\Pub\Controller\InlineMod {}
+	class XFCP_Post extends \XF\Pub\Controller\Post {}
+	class XFCP_ProfilePost extends \XF\Pub\Controller\ProfilePost {}
+	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
 }
