086cce08363b4253be7599215bd0b1a4ceecd5cf
TickTackk <me@ticktackk.io>
2019-01-08 01:42:17 +0530

    Remove bug fix not required in XF 2.0.10+

--- upload/src/addons/TickTackk/ChangeContentOwner/_output/extension_hint.php
+++ upload/src/addons/TickTackk/ChangeContentOwner/_output/extension_hint.php
@@ -10,13 +10,6 @@ namespace TickTackk\ChangeContentOwner\XFMG\Entity
 	class XFCP_MediaItem extends \XFMG\Entity\MediaItem {}
 }
 
-namespace TickTackk\ChangeContentOwner\XFMG\InlineMod
-{
-	class XFCP_Album extends \XFMG\InlineMod\Album {}
-	class XFCP_Comment extends \XFMG\InlineMod\Comment {}
-	class XFCP_Media extends \XFMG\InlineMod\Media {}
-}
-
 namespace TickTackk\ChangeContentOwner\XFMG\Pub\Controller
 {
 	class XFCP_Album extends \XFMG\Pub\Controller\Album {}
@@ -30,13 +23,6 @@ namespace TickTackk\ChangeContentOwner\XF\Entity
 	class XFCP_Post extends \XF\Entity\Post {}
 	class XFCP_ProfilePost extends \XF\Entity\ProfilePost {}
 	class XFCP_Thread extends \XF\Entity\Thread {}
-}
-
-namespace TickTackk\ChangeContentOwner\XF\InlineMod
-{
-	class XFCP_Post extends \XF\InlineMod\Post {}
-	class XFCP_ProfilePost extends \XF\InlineMod\ProfilePost {}
-	class XFCP_Thread extends \XF\InlineMod\Thread {}
 }
 
 namespace TickTackk\ChangeContentOwner\XF\Pub\Controller
