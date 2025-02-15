856df8c721ad4d0a4a35c7da1bab039bec776606
TickTackk <me@ticktackk.io>
2025-02-15 14:39:26 +0530

    Export options hint

---
+++ _output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property array{hours: int, minutes: int, seconds: int}|null $tckChangeContentOwner_defaultTimeInterval Default time interval
+ */
+class Options
+{
+}
